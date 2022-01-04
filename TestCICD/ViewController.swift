97b1d61a64b42893d61ee880c8d9d470e2ebfcf3
Mahmoud <Mahmoud.El-deeb@roqay.com.kw>
2022-01-04 15:39:23 +0200

    save

--- TestCICD/ViewController.swift
+++ TestCICD/ViewController.swift
@@ -9,6 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 //save
+
+    @IBAction func crashClicked(_ sender: Any) {
+        fatalError("Test CI/CD Issues")
+    }
+    
+    @IBAction func clickedup(_ sender: Any) {
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
